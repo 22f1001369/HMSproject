26b02e0ca88e7f8fc74c33260ab291925cf7b4bc
Your Name <you@example.com>
2025-10-30 17:55:56 +0530

    added vue cli

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,31 @@
 from flask import Flask
-from flask_restful import Api, Resource
 from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app)
 
 # connecting to database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-from models import db
+from models import db, User
 db.init_app(app)
 
-## This is what we were doing in mad1
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-
-# This is what we NEED to do in mad2 (i.e, returning data instead of HTML)
-class HelloWorld(Resource):
-    def get(self):
-        return {'message': 'Hello, World!'}
-    
-api.add_resource(HelloWorld, '/message')
+# connecting to routes
+from routes import api
+api.init_app(app)
 
 if __name__ == '__main__':
     
     with app.app_context():
+
         db.create_all()  # Create database tables for our data models
 
+        admin = User.query.filter_by(email='admin@gmail.com').first()
+
+        if not admin:
+            admin = User(email='admin@gmail.com', password='admin', role='admin')
+            db.session.add(admin); db.session.commit()
+            print("Admin user created with email: admin@gmail.com and password: admin")
+        else:
+            print("Admin user already exists with email: admin@gmail.com and password: admin")
+        
     app.run(debug=True)
